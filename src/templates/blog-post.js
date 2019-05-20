bc519e1d2afc885061e4d850080ac294be7d3135
daniel <daniel@thundermiracle.com>
2019-05-20 21:07:43 +0900

    customize layout, graphQL images from content/assets

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 
-import Bio from '../components/bio';
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+import Bio from '../components/Bio';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
 import { formatReadingTime } from '../utils/helpers';
 import { rhythm, scale } from '../utils/typography';
 
