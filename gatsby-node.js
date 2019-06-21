1334b4d700f34c6d607a2fa63c690ddff5bf406d
thundermiracle <cissoliu@gmail.com>
2019-06-21 20:13:13 +0900

    fix compile error

--- gatsby-node.js
+++ gatsby-node.js
@@ -117,16 +117,3 @@ exports.createPages = ({ graphql, actions }) => {
     );
   });
 };
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    });
-  }
-};
