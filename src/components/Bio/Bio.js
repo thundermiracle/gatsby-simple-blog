56c7f7939521560456a823466d38a04997503a4e
thundermiracle <cissoliu@gmail.com>
2019-07-02 22:22:45 +0900

    change bio design for better looking

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -9,6 +9,7 @@ import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 
+import './Bio.css';
 import { rhythm } from '../../utils/typography';
 
 import SocialBar from '../SocialBar';
@@ -26,28 +27,23 @@ function Bio() {
               marginBottom: rhythm(2.5),
             }}
           >
-            <div
-              style={{
-                display: `flex`,
-                alignItems: 'center',
-              }}
-            >
+            <div className="bio">
               <Image
                 fixed={data.avatar.childImageSharp.fixed}
                 alt={author}
+                className="pic"
                 style={{
                   marginRight: rhythm(1 / 2),
-                  marginBottom: 0,
-                  minWidth: 50,
-                  borderRadius: `100%`,
                 }}
                 imgStyle={{
-                  borderRadius: `50%`,
+                  borderRadius: '50%',
                 }}
               />
-              <p>{description}</p>
+              <div className="description">
+                <p>{description}</p>
+                <SocialBar />
+              </div>
             </div>
-            <SocialBar />
           </div>
         );
       }}
@@ -59,7 +55,7 @@ const bioQuery = graphql`
   query BioQuery {
     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
       childImageSharp {
-        fixed(width: 50, height: 50) {
+        fixed(width: 70, height: 70) {
           ...GatsbyImageSharpFixed
         }
       }
