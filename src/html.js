88f97f1012d882f98392ea88490d2bc216fabc40
thundermiracle <cissoliu@gmail.com>
2019-07-06 00:01:43 +0900

    fix typo

--- src/html.js
+++ src/html.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { themeOper, languageOper } from './utils/IIEF';
+import { themeOper, languageOper } from './utils/IIFE';
 
 function HTML({
   htmlAttributes,
