57e8d3d31f3714bf00da1b23b70637e883bb3792
thundermiracle <cissoliu@gmail.com>
2019-07-06 19:51:26 +0900

    refactor LanguageBar, get supportedLanguages from graphQL instead of config file

--- src/components/Layout/LanguageBar.js
+++ src/components/Layout/LanguageBar.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
 import './LanguageBar.css';
 
-import { supportedLanguages } from '../../../config';
 import { rhythm } from '../../utils/typography';
 import LangButton from '../LangButton';
 
@@ -12,30 +12,50 @@ import LangButton from '../LangButton';
  * @param {*object} { location, title, base}
  */
 function LanguageBar() {
-  if (supportedLanguages == null || Object.keys(supportedLanguages).length < 2) {
-    return null;
-  }
-
   const [lang, setLang] = useState('');
   useEffect(() => {
     const savedLang = window.__getPreferredLang();
     setLang(savedLang);
   });
 
-  if (!lang) {
-    return null;
-  }
-
   return (
-    <div
-      className="bar"
-      style={{
-        maxWidth: rhythm(24),
+    <StaticQuery
+      // eslint-disable-next-line no-use-before-define
+      query={supportedLanguagesQuery}
+      render={data => {
+        const { langsJson } = data.site.siteMetadata;
+        const supportedLanguages = JSON.parse(langsJson) || {};
+        if (supportedLanguages == null || Object.keys(supportedLanguages).length < 2) {
+          return null;
+        }
+
+        if (!lang) {
+          return null;
+        }
+
+        return (
+          <div
+            className="bar"
+            style={{
+              maxWidth: rhythm(24),
+            }}
+          >
+            <LangButton lang={lang} />
+          </div>
+        );
       }}
-    >
-      <LangButton lang={lang} />
-    </div>
+    />
   );
 }
+
+const supportedLanguagesQuery = graphql`
+  query SupportedLanguagesQuery {
+    site {
+      siteMetadata {
+        langsJson
+      }
+    }
+  }
+`;
 
 export default LanguageBar;
