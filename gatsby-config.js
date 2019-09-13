aa3ba270e4a6926bd872cd857c1b32e812159b12
thundermiracle <cissoliu@gmail.com>
2019-09-13 21:14:40 +0900

    open external-link in new tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,12 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+            },
+          },
         ],
       },
     },
