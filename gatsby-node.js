8cb0fac50f697af31f8a2e5a8a6e11347e7d7018
thundermiracle <cissoliu@gmail.com>
2021-04-24 16:43:02 +0900

    suppress warnings of minicssextractplugin, disable amp plugin;

--- gatsby-node.js
+++ gatsby-node.js
@@ -232,3 +232,16 @@ exports.onCreateNode = ({ node, actions }) => {
     });
   }
 };
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === 'build-javascript' || stage === 'develop') {
+    const config = getConfig();
+    const miniCssExtractPlugin = config.plugins.find(
+      (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin',
+    );
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true;
+    }
+    actions.replaceWebpackConfig(config);
+  }
+};
